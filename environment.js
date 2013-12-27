1fbbcd69ba70ec6606e34c8509ad1c655b008ba8
Elis Axelsson <elis.axelsson@gmail.com>
2013-12-27 12:33:54 +0100

    Base environment, setup of game, some styling, some markup, no player. Got running render loop, should add key-handling to the environment

---
+++ environment.js
@@ -0,0 +1,63 @@
+
+/**
+ * Environment class, keeps track of all game objects, canvases, pausing, unpausing
+ */
+var Environment = new Class({
+	Implements: [ Options ],
+	options: {
+		gameObjects: [],
+		width:  640,
+		height: 480,
+		pause:  false
+	},
+	/**
+	 * Set up environment
+	 */
+	initialize: function(options) {
+		env = this; // Set this to the global env variable
+
+		this.setOptions(options);
+
+		// Get the contexts
+		this.options.ctxs = {
+			background: $('background').getContext('2d'),
+			screen:     $('screen').getContext('2d')
+		}
+
+		this.drawFrame(); // Start gameLoop
+	},
+	/**
+	 * Loop where you should draw your stuff
+	 */
+	gameLoop: function() {
+		// Put things to draw here
+
+		this.drawFrame(); // Keep gameLoop running.
+	},
+	/**
+	 * Wrapper for the requestAnimationFrame request
+	 *
+	 * @TODO: Implement fallbacks if window.requestAnimationFrame doesn't exist
+	 */
+	drawFrame: function() {
+		if(!this.options.pause) {
+			window.requestAnimationFrame((function() {
+				this.gameLoop();
+			}).bind(this));
+		}
+	},
+	/**
+	 * Pause function
+	 */
+	pause: function() {
+		this.options.pause = true;
+	},
+	/**
+	 * Unpause function
+	 */
+	unpause: function() {
+		this.options.pause = false;
+		this.drawFrame();
+	}
+});
+
---
+++ game.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+var env, keyStates = {};
+
+/**
+ * On load, launch the game
+ */
+window.addEvent('load', function() {
+	new Environment();
+});
+
